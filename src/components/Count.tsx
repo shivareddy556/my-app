356ffbd97cbb54f8a5e9954d0f97f1fcf0ae3311
shivareddy556 <sivasiva36475@mail.com>
2024-05-09 09:36:57 +0530

    replaced code count code to reducer

--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -1,59 +1,93 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react';
 import Button from '@mui/material/Button';
-import '../App.css'
+import '../App.css';
+
+const initialState = {
+  count: 0,
+  inputValue: ''
+};
+
+const reducer = (state: { count: number; inputValue: string; }, action: { type: any; payload: any; }) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, count: state.count + action.payload };
+    case 'INCREMENT_RANDOM':
+      return { ...state, count: state.count + Math.floor(Math.random() * 10) + 1 };
+    case 'INCREMENT_TO_ODD':
+      return { ...state, count: state.count % 2 === 0 ? state.count + 1 : state.count + 2 };
+    case 'DECREMENT_BY_INPUT':
+      const decrementValue = parseInt(state.inputValue);
+      return !isNaN(decrementValue)
+        ? { ...state, count: state.count - decrementValue, inputValue: '' }
+        : state;
+    case 'RESET':
+      return { ...state, count: 0 };
+    case 'SET_INPUT_VALUE':
+      return { ...state, inputValue: action.payload };
+    default:
+      return state;
+  }
+};
+
 const Count = () => {
-    const [count, setCount] = useState(0);
-    const [inputValue, setInputValue] = useState('');
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    const handleIncrement = () => {
-        setCount(prevCount => prevCount + 1);
-    };
+  const handleIncrement = () => {
+    dispatch({ type: 'INCREMENT', payload: 1 });
+  };
 
-    const handleIncrementRandom = () => {
-        const randomIncrement = Math.floor(Math.random() * 10) + 1;
-        setCount(prevCount => prevCount + randomIncrement);
-    };
+  const handleIncrementRandom = () => {
+    dispatch({
+        type: 'INCREMENT_RANDOM',
+        payload: null
+    });
+  };
 
-    const handleIncrementToOdd = () => {
-        setCount(prevCount => prevCount % 2 === 0 ? prevCount + 1 : prevCount + 2);
-    };
+  const handleIncrementToOdd = () => {
+    dispatch({
+        type: 'INCREMENT_TO_ODD',
+        payload: null
+    });
+  };
 
-    const handleDecrementByInput = () => {
-        const decrementValue = parseInt(inputValue);
-        if (!isNaN(decrementValue)) {
-            setCount(prevCount => prevCount - decrementValue);
-            setInputValue('');
-        }
-    };
+  const handleDecrementByInput = () => {
+    dispatch({
+        type: 'DECREMENT_BY_INPUT',
+        payload: null
+    });
+  };
 
-    const handleReset = () => {
-        setCount(0);
-    };
+  const handleReset = () => {
+    dispatch({
+        type: 'RESET',
+        payload: null
+    });
+  };
 
-    const handleInputChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-        setInputValue(e.target.value);
-    };
+  const handleInputChange = (e: { target: { value: any; }; }) => {
+    dispatch({ type: 'SET_INPUT_VALUE', payload: e.target.value });
+  };
 
-    return (
-        <>
-            <div className='center-items'>
-            <div >
-                <h1 className='input-div-styles'>Count : &nbsp;  <span className='count_color'> {count}</span></h1>
-                <Button size="small" variant="contained" color="secondary" onClick={handleIncrement}>Increment</Button> &nbsp;
-                <Button size="small" variant="contained" color="success" onClick={handleIncrementRandom}>Increment Randomly</Button> &nbsp;
-                <Button size="small" variant="contained" color='secondary' onClick={handleIncrementToOdd}>Increment to Odd</Button>
-                <p> 
-                    <div>This button decrements the count by the value entered in the input field :</div>
-                   <div>
-                   <input type="text" className='input-styles' value={inputValue} onChange={handleInputChange} /> &nbsp;
-                <Button size="small" variant="contained" onClick={handleDecrementByInput}>Decrement by Input</Button>
-                   </div>
-                   </p>
-               <div> Reset The Count: <Button size="small" variant="contained" color="secondary" onClick={handleReset}>Reset</Button></div>
+  return (
+    <>
+      <div className='center-items'>
+        <div>
+          <h1 className='input-div-styles'>Count : &nbsp;  <span className='count_color'> {state.count}</span></h1>
+          <Button size="small" variant="contained" color="secondary" onClick={handleIncrement}>Increment</Button> &nbsp;
+          <Button size="small" variant="contained" color="success" onClick={handleIncrementRandom}>Increment Randomly</Button> &nbsp;
+          <Button size="small" variant="contained" color='secondary' onClick={handleIncrementToOdd}>Increment to Odd</Button>
+          <p> 
+            <div>This button decrements the count by the value entered in the input field :</div>
+            <div>
+              <input type="text" className='input-styles' value={state.inputValue} onChange={handleInputChange} /> &nbsp;
+              <Button size="small" variant="contained" onClick={handleDecrementByInput}>Decrement by Input</Button>
             </div>
-            </div>
-        </>
-    )
+          </p>
+          <div> Reset The Count: <Button size="small" variant="contained" color="secondary" onClick={handleReset}>Reset</Button></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Count
+export default Count;
